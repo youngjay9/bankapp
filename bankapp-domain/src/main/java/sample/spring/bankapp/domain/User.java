abaadeec6622b8032feedbcaaa372fbefc7d5cda
Jay Hung <youngjay9@gmail.com>
2019-03-17 10:58:25 +0800

    add bankapp-domain user

---
+++ bankapp-domain/src/main/java/sample/spring/bankapp/domain/User.java
@@ -0,0 +1,25 @@
+package sample.spring.bankapp.domain;
+
+public class User {
+	
+	private long id;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	private String name;
+
+}
