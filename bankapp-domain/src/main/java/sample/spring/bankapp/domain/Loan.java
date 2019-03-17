7ad9e86aac43fa677382662ea40d49c96a67edcf
Jay Hung <youngjay9@gmail.com>
2019-03-17 11:22:03 +0800

    bankapp-domain add Loan object

---
+++ bankapp-domain/src/main/java/sample/spring/bankapp/domain/Loan.java
@@ -0,0 +1,15 @@
+package sample.spring.bankapp.domain;
+
+public class Loan {
+	
+	private Long lonNo;
+
+	public Long getLonNo() {
+		return lonNo;
+	}
+
+	public void setLonNo(Long lonNo) {
+		this.lonNo = lonNo;
+	}
+
+}
